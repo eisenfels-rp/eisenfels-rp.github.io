0447f31c2d0fa10c36dd84960b29d6992cc33f6f
eisenfels-rp <entropolisrechtsamt@gmail.com>
2026-05-03 22:41:03 +0200

    Update admin-config.js

--- MephMK/admin-config.js
+++ MephMK/admin-config.js
@@ -1,5 +1,5 @@
 window.EISENFELS_ADMIN_CONFIG = {
-  passwordHash: "7d3815a30fa31a746d44f011d2dcf8e3435a2ebd84f36aca19e895d9ab91522f",
+  passwordHash: "8b01885d3945caa135ca0b11b06d053135003f2fc9838e887f3f21f2c12f6eee",
   repoOwner: "eisenfels-rp",
   repoName: "eisenfels-rp.github.io",
   branch: "main",
